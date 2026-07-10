304700f0798a292c283940677b0801c4206e1e37
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-07-10 23:20:18 +0700

    add more model relate to users

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -3,3 +3,13 @@ from pydantic import BaseModel
 class UserIn(BaseModel):
     username: str
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class UserDB(BaseModel):
+    hashed_password: str
