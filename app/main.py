fd335a63e765406091f18af3239016cece372f24
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-07-11 01:02:42 +0700

    include users router

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from app.database.database import init_db
-from app.routers import habits, habit_records
+from app.routers import habits, habit_records, users
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,3 +13,4 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(habits.router)
 app.include_router(habit_records.router)
+app.include_router(users.router)
