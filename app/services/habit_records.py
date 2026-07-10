47f139ca209a0cb8df9613d7dc2a3aa13dc85448
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-07-10 10:57:02 +0700

    add get_records in service

--- app/services/habit_records.py
+++ app/services/habit_records.py
@@ -1,5 +1,7 @@
 from sqlalchemy.orm import Session
 
+from sqlalchemy import select
+
 from app.models.models import HabitRecord
 from app.schemas.habit_records import CreateHabitRecord
 
@@ -10,4 +12,11 @@ def create_record(
     habit_record = HabitRecord(**create_habit_record.model_dump())
     db.add(habit_record)
     db.commit()
-    
+
+def get_records(
+    db: Session,
+    habit_id: int
+):
+    stmt = select(HabitRecord).where(HabitRecord.habit_id==habit_id)
+    results = db.execute(stmt).scalars().all()
+    return results
