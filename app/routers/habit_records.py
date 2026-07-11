4e2e1e3902a6fb88474abd3e85023f0810b54636
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-07-11 21:25:58 +0700

    add user to habit_records route

--- app/routers/habit_records.py
+++ app/routers/habit_records.py
@@ -1,6 +1,9 @@
 from typing import Annotated
 from fastapi import APIRouter, Path, Body, Depends, status, HTTPException
 from app.dependencies import get_db
+from app.dependencies import get_current_user
+
+from app.models.models import User
 
 from app.schemas.habit_records import CreateHabitRecord
 from sqlalchemy.orm import Session
@@ -15,21 +18,25 @@ router = APIRouter(
 @router.post('/create')
 def create_habit_records(
     db: Annotated[Session, Depends(get_db)],
-    create_habit_record: Annotated[CreateHabitRecord, Body()]   
+    create_habit_record: Annotated[CreateHabitRecord, Body()],
+    current_user: Annotated[User, Depends(get_current_user)],
 ):
     habit_records_service.create_record(
         db,
-        create_habit_record
+        create_habit_record,
+        current_user.id,
     )
 
 @router.get('/{habit_id}')
 def get_habit_records(
     db: Annotated[Session, Depends(get_db)],
     habit_id: int,
+    current_user: Annotated[User, Depends(get_current_user)],
 ):
-    results = habit_records_service.get_records(
+    results = habit_records_service.get_all_records_from_habit(
         db,
-        habit_id
+        habit_id,
+        current_user.id,
     )
     return results
 
@@ -37,13 +44,15 @@ def get_habit_records(
 def modify_habit_records(
     db: Annotated[Session, Depends(get_db)],
     record_id: Annotated[int, Path()],
-    record: Annotated[CreateHabitRecord, Body()]
+    record: Annotated[CreateHabitRecord, Body()],
+    current_user: Annotated[User, Depends(get_current_user)],
 ):
     try:
         habit_records_service.modify_record(
             db,
             record_id,
-            record
+            record,
+            current_user.id,
         )
     except ValueError as e:
         raise HTTPException(
@@ -54,12 +63,14 @@ def modify_habit_records(
 @router.delete('/{record_id}')
 def delete_habit_record(
     db: Annotated[Session, Depends(get_db)],
-    record_id: Annotated[int, Path()]
+    record_id: Annotated[int, Path()],
+    current_user: Annotated[User, Depends(get_current_user)],
 ):
     try:
         habit_records_service.delete_record(
             db,
-            record_id
+            record_id,
+            current_user.id,
         )
     except ValueError as e:
         raise HTTPException(
--- app/services/habit_records.py
+++ app/services/habit_records.py
@@ -2,22 +2,37 @@ from sqlalchemy.orm import Session
 
 from sqlalchemy import select
 
-from app.models.models import HabitRecord
+from app.models.models import HabitRecord, Habit
 from app.schemas.habit_records import CreateHabitRecord
 
 def create_record(
     db: Session,
-    create_habit_record: CreateHabitRecord
+    create_habit_record: CreateHabitRecord,
+    user_id: int
 ):
-    habit_record = HabitRecord(**create_habit_record.model_dump())
-    db.add(habit_record)
-    db.commit()
-
-def get_records(
+    #find habit
+    stmt = select(Habit)\
+        .where(Habit.id==create_habit_record.habit_id)
+    habit = db.execute(stmt).scalar_one()
+    if not habit:
+        raise ValueError(f'Habit {create_habit_record.habit_id} does not exist')
+    elif habit.user_id == user_id:
+        habit_record = HabitRecord(**create_habit_record.model_dump())
+        db.add(habit_record)
+        db.commit()
+    else:
+        raise ValueError(f'Wrong user_id')
+    
+def get_all_records_from_habit(
     db: Session,
-    habit_id: int
+    habit_id: int,
+    user_id,
 ):
-    stmt = select(HabitRecord).where(HabitRecord.habit_id==habit_id)
+    stmt = select(HabitRecord).\
+        select_from(HabitRecord)\
+            .join(Habit, HabitRecord.habit_id==Habit.id)\
+                .where(HabitRecord.habit_id==habit_id)\
+                    .where(Habit.user_id==user_id)
     results = db.execute(stmt).scalars().all()
     return results
 
@@ -25,8 +40,13 @@ def modify_record(
     db: Session,
     record_id: int,
     new_record: CreateHabitRecord,
+    user_id: int,
 ):
-    stmt = select(HabitRecord).where(HabitRecord.id==record_id)
+    stmt = select(HabitRecord)\
+        .select_from(HabitRecord)\
+            .join(Habit, HabitRecord.habit_id==Habit.id)\
+                .where(Habit.user_id==user_id)\
+                    .where(Habit.id==record_id)
     result = db.execute(stmt).first()
     if result is None:
         raise ValueError(f'Habit Record {record_id} does not exist')
@@ -39,10 +59,16 @@ def modify_record(
 
 def delete_record(
     db: Session,
-    record_id: int
+    record_id: int,
+    user_id: int,
 ):
+    stmt = select(HabitRecord)\
+        .select_from(HabitRecord)\
+            .join(Habit, HabitRecord.habit_id==Habit.id)\
+                .where(Habit.user_id==user_id)\
+                    .where(HabitRecord.id==record_id)
     record = db.scalars(
-        select(HabitRecord).where(HabitRecord.id==record_id)
+        stmt
     ).one()
     
     if not record:
